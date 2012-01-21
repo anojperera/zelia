749a46113f84612092e01b365f5972c55c57cb9a
Anoj Perera <anojmperera@gmail.com>
2012-01-21 17:24:12 +0000

    zBase class added.
    This class shall be used to derive all component classes

---
+++ src/zBase.c
@@ -0,0 +1,124 @@
+/* Implementation of zBase class
+ * Sat Jan 21 16:23:09 GMT 2012 */
+
+#include "zBase.h"
+
+/* Virtual function call back */
+static int zbase_draw_function(zGeneric* obj);
+
+
+/* Constructor */
+zGeneric* zBase_New(zBase* obj)
+{
+    if(obj == NULL)
+	{
+	    obj = (zBase*) malloc(sizeof(zBase));
+	    Z_CHECK_OBJ_PTR(obj);
+	    obj->z_int_flg = 1;
+	}
+    else
+	obj->z_int_flg = 0;
+
+    /* create parent object */
+    zGeneric_New(&obj->z_sgeneric)
+
+    /* set properties */
+    obj->z_x = 0.0;
+    obj->z_y = 0.0;
+    obj->z_ang = 0.0;
+    obj->z_width = 0.0;
+    obj->z_height = 0.0;
+
+    obj->z_draw_func = NULL;
+
+    /* assigne function pointer of base object */
+    obj->z_sgeneric.z_draw_func = zbase_draw_function;
+
+    /* set child pointer of parent object */
+    obj->z_sgeneric.z_child = (void*) obj;
+
+    /* return base pointer */
+    return &obj->z_sgeneric;
+}
+
+/* Destructor */
+void zBase_Delete(zBase* obj)
+{
+    obj->z_draw_func = NULL;
+
+    zGeneric_Delete(&obj->z_sgeneric);
+
+    if(obj->z_int_flg)
+	free(obj);
+
+    return;
+}
+
+/* Set base coordinates */
+inline int zBase_Set_Base_Coords(zBase* obj, double x, double y)
+{
+    /* check for object pointers */
+    Z_CHECK_OBJ(obj);
+    obj->z_x = x;
+    obj->z_y = y;
+    return 0;
+}
+
+/* Set orientation angle */
+inline int zBase_Set_Orientation(zBase* obj, double ang)
+{
+    Z_CHECK_OBJ(obj);
+    obj->z_ang = ang;
+    return 0;
+}
+
+/* Get orientation angle */
+inline double zBase_Get_Orientation(zBase* obj)
+{
+    Z_CHECK_OBJ_DOUBLE(obj);
+    return obj->z_ang;
+}
+
+/* Set width */
+inline int zBase_Set_Width(zBase* obj, double width)
+{
+    /* check object */
+    Z_CHECK_OBJ(obj);
+    obj->z_width = width;
+    return 0;
+}
+
+/* Get width */
+inline double zBase_Get_Width(zBase* obj)
+{
+    Z_CHECK_OBJ_DOUBLE(obj);
+    return obj->width;
+}
+
+/* Set height */
+inline int zBase_Get_Height(zBase* obj, double height)
+{
+    Z_CHECK_OBJ(obj);
+    obj->z_height = height;
+    return 0;
+}
+
+/* Get height */
+inline double zBase_Get_Height(zBase* obj)
+{
+    Z_CHECK_OBJ_DOUBLE(obj);
+    return obj->z_height;
+}
+
+/*==================================================================*/
+/* Private functions */
+
+/* Draw function */
+static int zbase_draw_function(zGeneric* obj)
+{
+    Z_CHECK_OBJ(obj);
+    zBase* base = Z_BASE(obj);
+    Z_CHECK_OBJ(base->z_draw_func);
+    return base->z_draw_func(obj);
+}
+
---
+++ src/zBase.h
@@ -0,0 +1,57 @@
+/* Base object for deriving all component object,
+ * Describes, primitive coordinates */
+/* Sat Jan 21 16:05:52 GMT 2012 */
+
+#ifndef _ZBASE_H_
+#define _ZBASE_H_
+
+#include <stdlib.h>
+#include "zGeneric.h"
+
+typedef struct _zBase zBase;
+
+struct _zBase
+{
+    zGeneric z_sgeneric;			/* inherited parent object */
+    unsigned int z_int_flg;			/* internal flag */
+    /* base coordinates */
+    double z_x;
+    double z_y;
+    double z_ang;				/* orientation angle */
+    double z_width;				/* with */
+    double z_height;				/* height */
+
+    zgeneric_fptr z_draw_func;			/* draw function */
+};
+
+#ifdef __cplusplus
+extern "C" {
+#ifdef
+
+    /* Constructor and destructor */
+    zGeneric* zBase_New(zBase* obj);
+    void zBase_Delete(zBase* obj);
+
+    /* Property functions */
+    /*************************************************************/
+
+    /* Set base coordinates */
+    inline int zBase_Set_Base_Coords(zBase* obj, double x, double y);
+
+    /* Set orientation angle */
+    inline int zBase_Set_Orientation(zBase* obj, double ang);
+    inline double zBase_Get_Orientation(zBase* obj);
+
+    /* Set and Get width height */
+    inline int zBase_Set_Width(zBase* obj, double width);
+    inline double zBase_Get_Width(zBase* obj);
+
+    inline int zBase_Get_Height(zBase* obj, double height);
+    inline double zBase_Get_Height(zBase* obj);
+    
+
+#ifdef __cplusplus
+}
+#ifdef
+
+#endif /* _ZBASE_H_ */
