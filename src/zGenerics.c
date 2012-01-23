815434e75d9351a4e3e8c8c3c8304cd19c82fb60
Anoj Perera <anojmperera@gmail.com>
2012-01-23 00:43:19 +0000

    Constructor complete

--- src/zGenerics.c
+++ src/zGenerics.c
@@ -18,6 +18,38 @@ int zGenerics_New(zGenerics** obj,
 	    return 1;
 	}
 
-    
+    /* Create object if NULL */
+    if(*obj == NULL)
+	{
+	    *obj = (zGenerics*) malloc(sizeof(zGenerics));
+	    Z_CHECK_OBJ(*obj);
+	}
 
+    /* Set properties */
+    (*obj)->z_generics_d = NULL;
+
+    /* if static flag and count was set create the object array
+     * as static */
+    if(s_flg == 0 && g_count > 0)
+	{
+	    (*obj)->z_generics_s = (zGeneric**)
+		calloc(g_count, sizeof(zGeneric*));
+
+	    /* initialise all objects in array to NULL */
+	    int i = 0;
+	    for(i; i<g_count; i++)
+		(*obj)->z_generics_s[i] = NULL;
+
+	    /* set the expansion flag to 0 - indicating not dynamically
+	     * expanding */
+	    (*obj)->z_expansion_flg = 0;
+	}
+    
+    /* set function pointer to NULL */
+    (*obj)->z_draw_func = NULL;
+
+    /* set child pointer to NULL */
+    (*obj)->z_child = NULL;
+    
+    return 0;
 }
