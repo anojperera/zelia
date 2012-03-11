be8b91ffcafff754250f9e11678e55a66e9d533a
Anoj Perera <anojmperera@gmail.com>
2012-03-11 14:01:43 +0000

    zDFrame conversion macro added.

--- src/zVar.h
+++ src/zVar.h
@@ -199,6 +199,7 @@
 #define Z_TABLE(obj) ((zTable*) Z_BASE(obj)->z_child)
 #define Z_NOTE(obj) ((zNote*) Z_BASE(obj)->z_child)
 #define Z_NOTES(obj) ((zNotes*) obj->z_child)
+#define Z_DFRAME(obj) ((zDFrame*) Z_BASE(obj)->z_child)
 
 /* Object checking macro */
 #define Z_CHECK_OBJ(obj) \
