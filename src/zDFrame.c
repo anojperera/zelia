0522c08a33b86dec1e319016848586f80e587068
Anoj Perera <anojmperera@gmail.com>
2012-03-11 14:01:32 +0000

    zDFrame - class added
    zDFrame class is a generic frame class. All frame components shall be
    derived from this class.

---
+++ src/zDFrame.c
@@ -0,0 +1,128 @@
+/* Implementation of zDFrame class */
+/* Sun Mar 11 13:45:41 GMT 2012 */
+
+#include "zDFrame.h"
+#include "zVar.h"
+
+/* Virtual functions */
+static int _zddrame_draw(zGeneric* obj);
+
+/* Constructor */
+zGeneric* zDFrame_New(zDFrame* obj)
+{
+    /* constructor helper */
+    Z_CONSTRUCTOR_HELPER(obj, zDFrame);
+
+    /* create parent object */
+    if(!zBase_New(&obj->z_parent))
+	{
+	    if(obj->z_int_flg)
+		free(obj);
+	    return NULL;
+	}
+
+    /* Set properties */
+    obj->z_rtlip_flg = 0;
+    obj->z_frm_type = 0;		/* FD frame */
+    obj->z_fillet_flg = 1;		/* Fillet as required */
+    obj->z_draw_func = NULL;
+    obj->z_child = NULL;
+
+    /* Set function pointers and child pointer of parent
+     * object */
+    obj->z_parent.z_draw_func = _zddrame_draw;
+    obj->z_parent.z_child = (void*) obj;
+
+    /* Return parent object */
+    return &obj->z_parent.z_sgeneric;
+}
+
+/* Destructor */
+void zDFrame_Delete(zDFrame* obj)
+{
+    /* check object */ 
+    Z_CHECK_OBJ_VOID(obj);
+
+    /* Delete parent object */
+    zBase_Delete(&obj->z_parent);
+
+    obj->z_draw_func = NULL;
+    obj->z_child = NULL;
+
+    /* If internally created, free memory */
+    if(obj->z_int_flg)
+	free(obj);
+}
+
+/**********************************************************************/
+/* Property methods */
+
+/* Set return lip flag */
+inline int zDFrame_Set_Return_Lip_Flg(zDFrame* obj, unsigned int flg)
+{
+    Z_CHECK_OBJ(obj);
+    obj->z_rtlip_flg = flg;
+    return 0;
+}
+
+/* Get return lip flag */
+inline unsigned int zDFrame_Get_Return_Lip_Flg(zDFrame* obj)
+{
+    if(obj == NULL)
+	return 0;
+
+    return obj->z_rtlip_flg;
+}
+
+/* Set frame type */
+inline int zDFrame_Set_Frame_Type(zDFrame* obj, unsigned int ztype)
+{
+    Z_CHECK_OBJ(obj);
+    obj->z_frm_type = flg;
+    return 0;
+}
+
+/* Get frame type */
+inline unsigned int zDFrame_Get_Frame_Type(zDFrame* obj)
+{
+    if(obj == NULL)
+	return 0;
+
+    return obj->z_frm_type;
+}
+
+/* Set frame fillet flag */
+inline int zDFrame_Set_Fillet_Flg(zDFrame* obj, unsigned int flg)
+{
+    Z_CHECK_OBJ(obj);
+    obj->z_fillet_flg = flg;
+    return 0;
+}
+
+/* Get frame fillet flag */
+inline unsigned int zDFrame_Get_Fillet_Flg(zDFrame* obj)
+{
+    if(obj == NULL)
+	return 0;
+    return obj->z_fillet_flg;
+}
+
+/****************************************************************************/
+/* Private draw functions */
+
+/* Virtual draw functions */
+static int _zddrame_draw(zGeneric* obj)
+{
+    zDFrame* _dfrm;
+    
+    /* Check object */
+    Z_CHECK_OBJ(obj);
+
+    _dfrm = Z_DFRAME(obj);
+    /* Check if function poitner was
+     * assigned. */
+    if(_dfrm->z_draw_func)
+	return _dfrm->z_draw_func(obj);
+    else
+	return 1;
+}
---
+++ src/zDFrame.h
@@ -0,0 +1,59 @@
+/* Generic damper frame class. Provides interfaces child
+ * side, top and bottom, mullion and transom frames.
+ * This class shall be used for deriving the above child
+ * classes. Inherits base class.
+ * Sun Mar 11 13:34:18 GMT 2012 */
+
+#ifndef _ZDFRAME_H_
+#define _ZDFRAME_H_
+
+#include <stdlib.h>
+#include "zGeneric.h"
+#include "zBase.h"
+
+/* Forward declaration of struct */
+typedef struct _zDFrame zDFrame;
+
+struct _zDFrame
+{
+    zBase z_parent;				/* Inherited parent class */
+    unsigned int z_int_flg;			/* Internal flag */
+    unsigned int z_rtlip_flg;			/* Flag to indicate return lip */
+    unsigned int z_frm_type;			/* Frame type - FD or ND
+    						 * 0 - FD frame
+						 * 1 - ND frame */
+    
+    unsigned int z_fillet_flg;			/* Flag to indicate fillet the frame */    
+    zgeneric_fptr z_draw_func;			/* Draw function */
+    void* z_child;				/* Child pointer */
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /* Constructor and Destructor */
+    zGeneric* zDFrame_New(zDFrame* obj);
+    void zDFrame_Delete(zDFrame* obj);
+
+    /* Draw methods to be defined in child classes */
+
+    /***********************************************/
+    /* Property methods */
+
+    /* Set and get return lip flag */
+    inline int zDFrame_Set_Return_Lip_Flg(zDFrame* obj, unsigned int flg);
+    inline unsigned int zDFrame_Get_Return_Lip_Flg(zDFrame* obj);
+
+    /* Set and get frame type */
+    inline int zDFrame_Set_Frame_Type(zDFrame* obj, unsigned int ztype);
+    inline unsigned int zDFrame_Get_Frame_Type(zDFrame* obj);
+
+    /* Set and get fillet type flag */
+    inline int zDFrame_Set_Fillet_Flg(zDFrame* obj, unsigned int flg);
+    inline unsigned int zDFrame_Get_Fillet_Flg(zDFrame* obj);
+#ifdef __cplusplus
+}
+#endif    
+
+#endif /* _ZDFRAME_H_ */
