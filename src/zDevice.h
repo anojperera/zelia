87ea02184bf37945833d30168168d6ba370c3d2e
Anoj Perera <anojmperera@gmail.com>
2013-08-06 22:32:49 +0100

    Property methods moved to header fill

--- src/zDevice.h
+++ src/zDevice.h
@@ -17,41 +17,41 @@
 
 #include "zVar.h"
 
+#define Z_DEVICE_FNAME_SZ 256
+#define Z_DEVICE_PAGE_STR_SZ 16
+/* forward declaration */
+typedef struct _zDevice zDevice;
+
+struct _zDevice
+{
+    unsigned int var_int_flg;			/* internal flag */
+    cairo_surface_t* z_surface;			/* cairo surface */
+    cairo_t* z_device;				/* cairo default device */
+
+    zLineColour z_line_color_ix;		/* line color index */
+    zSheets z_page_sz;				/* page size */
+    zOutputFormat z_format;			/* out put format */
+    char z_filename[Z_DEVICE_FNAME_SZ];		/* file name */
+    char z_page_sz_str[Z_DEVICE_PAGE_STR_SZ];	/* page size string */
+    double z_red_rgb;				/* rgb red */
+    double z_green_rgb;				/* rgb green */
+    double z_blue_rgb;				/* rgb blue */
+
+    double z_page_width;			/* page width */
+    double z_page_height;			/* page height */
+    size_t z_obj_sz;				/* object size */
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-
-    /* forward declaration */
-    typedef struct _zDevice zDevice;
-
-    struct _zDevice
-    {
-	unsigned int var_int_flg;		/* internal flag */
-	cairo_surface_t* z_surface;		/* cairo surface */
-	cairo_t* z_device;			/* cairo default device */
-
-	zLineColour z_line_color_ix;		/* line color index */
-	zSheets z_page_sz;			/* page size */
-	zOutputFormat z_format;			/* out put format */
-	char z_filename[256];			/* file name */
-	char z_page_sz_str[16];			/* page size string */
-	double z_red_rgb;			/* rgb red */
-	double z_green_rgb;			/* rgb green */
-	double z_blue_rgb;			/* rgb blue */
-
-	double z_page_width;			/* page width */
-	double z_page_height;			/* page height */
-	size_t z_obj_sz;			/* object size */
-    };
-
     /* constructor and destructor */
     zDevice* zDevice_New(zDevice* obj);
     zDevice* zDevice_New2(zOutputFormat fm,
-			     zSheets sh,
-			     const char* fn,
-			     int dev_flg,
-			     zDevice* obj);
+			  zSheets sh,
+			  const char* fn,
+			  int dev_flg,
+			  zDevice* obj);
 	
     void zDevice_Delete(zDevice* obj);
 
@@ -61,28 +61,91 @@ extern "C" {
     /* Set and get type of surface */
     inline int zDevice_Set_SurfaceType(zDevice* obj,
 				       zOutputFormat var);
-    inline zOutputFormat zDevice_Get_SurfaceType(zDevice* obj);
+    inline __attribute__ ((always_inline)) static zOutputFormat zDevice_Get_SurfaceType(zDevice* obj)
+    {
+	if(obj == NULL)
+	    return zFormatPDF;
+	else
+	    return obj->z_format;
+    }
 
     /* Set and get page size */
-    inline int zDevice_Set_PageSize(zDevice* obj,
-				    zSheets var);
-    inline zSheets zDevice_Get_PageSize(zDevice* obj);
+    inline __attribute__ ((always_inline)) static int zDevice_Set_PageSize(zDevice* obj, zSheets var)
+    {
+	/* check for NULL pointer */
+	Z_CHECK_OBJ(obj);
+
+	obj->z_page_sz = var;
+	zdevice_page_dims(obj);
+	return 0;
+    }
+
+    /* get page size */
+    inline __attribute__ ((always_inline)) static zSheets zDevice_Get_PageSize(zDevice* obj)
+    {
+	if(obj == NULL)
+	    return zSheetA3_Landscape;
+	else
+	    return obj->z_page_sz;
+    }
 
     /* Returns a pointer to page size string */
-    inline char* zDevice_Get_PageSizeStr(zDevice* obj);
+    inline __attribute__ ((always_inline)) static char* zDevice_Get_PageSizeStr(zDevice* obj)
+    {
+	/* check for NULL pointer */
+	Z_CHECK_OBJ_PTR(obj);
+	return obj->z_page_sz_str;
+    }
 
     /* Set and get file name */
-    inline int zDevice_Set_FileName(zDevice* obj,
-				    const char* var);
-    inline char* zDevice_Get_FileName(zDevice* obj);
+    inline __attribute__ ((always_inline)) static int zDevice_Set_FileName(zDevice* obj, const char* var)
+    {
+	/* check for object NULL pointer */
+	Z_CHECK_OBJ(obj);
+	Z_CHECK_OBJ(var);
+
+	strncpy(obj->z_filename, var, Z_DEVICE_FNAME_SZ);
+
+	return 0;
+    }
+
+    /* Get file name */
+    inline __attribute__ ((always_inline)) static char* zDevice_Get_FileName(zDevice* obj)
+    {
+	Z_CHECK_OBJ_PTR(obj);
+	return obj->z_filename;
+    }
 		
     /* Get default device context */
-    inline cairo_t* zDevice_Get_Context(zDevice* obj); 
+    inline __attribute__ ((always_inline)) static cairo_t* zDevice_Get_Context(zDevice* obj)
+    {
+	/* check for NULL pointer */
+	Z_CHECK_OBJ_PTR(obj);
+
+	zdevice_create_context(obj);
+	return obj->z_device;
+    }
 
     /* Set and get line colour */
-    inline int zDevice_Set_LineColourIx(zDevice* obj,
-					zLineColour var);
-    inline zLineColour zDevice_Get_LineColourIx(zDevice* obj); 
+    inline __attribute__ ((always_inline)) static int zDevice_Set_LineColourIx(zDevice* obj, zLineColour var)
+    {
+	/* check for NULL pointer */
+	Z_CHECK_OBJ(obj);
+
+	obj->z_line_color_ix = var;
+	zdevice_rgb(obj);
+	return 0;
+    }
+
+    /* get line color index */
+    inline __attribute__ ((always_inline)) static zLineColour zDevice_Get_LineColourIx(zDevice* obj)
+    {
+	/* check for NULL pointer */
+	if(obj == NULL)
+	    return zLBlack;
+	else
+	    return obj->z_line_color_ix;
+    }
 
 	
 #ifdef __cplusplus
