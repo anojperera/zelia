cfd935cb404938d55d971d0f12dc7f8d8491c01c
Anoj Perera <anoj.perera@cloudmargin.com>
2014-11-27 19:25:36 +0000

    Base coordinate class added.
    Signed-off-by: Anoj Perera <anojmperera@gmail.com>

---
+++ src/zbase.c
@@ -0,0 +1,78 @@
+/* Implementation of zBase class
+ * Sat Jan 21 16:23:09 GMT 2012 */
+
+#include "zbase.h"
+
+/* Virtual function call back */
+static int zbase_draw_function(void* obj);
+
+
+/* Constructor */
+zgeneric* zbase_new(zbase* obj)
+{
+	/* call constructor helper */
+	ZCONSTRUCTOR(obj, zbase);
+
+    /* create parent object */
+    obj->super_cls = zgeneric_new(&obj->parent);
+
+    /* set properties */
+    obj->x = 0.0;
+    obj->y = 0.0;
+    obj->ang = 0.0;
+    obj->width = 0.0;
+    obj->height = 0.0;
+    obj->thk = 0.0;
+    obj->prj_flg = 0;
+    obj->child = NULL;
+
+	/* initialise vtable */
+	ZGENERIC_INIT_VTABLE(obj);
+	
+	/* set parent objects function pointers */
+	zgeneric_set_draw(obj, zbase_draw_function);
+	
+    /* set child pointer of parent object */
+	zgeneric_set_child_pointer(obj);
+
+    /* return base pointer */
+    return obj->super_cls;
+}
+
+/* Destructor */
+void zbase_delete(zbase* obj)
+{
+	ZCHECK_OBJ_VOID(obj);
+	/* if function pointer was set call it */
+	if(obj->vtable.zgeneric_delete)
+		obj->vtable.zgeneric_delete((void*) obj->super_cls);
+
+	obj->child = NULL;
+    zgeneric_delete(&obj->sgeneric);
+
+	/* check if the object was constructed */
+	if(ZDESTRUCTOR_CHECK)
+		free(obj);
+	
+    return;
+}
+
+/*=================================== Private Methods ===================================*/
+
+/* draw function */
+static int zbase_draw_function(void* obj)
+{
+	zgeneric* _zg = NULL;
+    zbase* self = NULL;
+
+    ZCHECK_OBJ_INT(obj);
+	_zg = (zgeneric*) obj;
+	
+    self = Z_BASE(_zg);
+	
+    if(self->vtable.zgeneric_draw)
+    	return self->vtable.zgeneric_draw(obj);
+    else
+		return ZELIA_OK;
+}
+
---
+++ src/zbase.h
@@ -0,0 +1,133 @@
+/* Base object for deriving all component object,
+ * Describes, primitive coordinates */
+/* Sat Jan 21 16:05:52 GMT 2012 */
+
+#ifndef _ZBASE_H_
+#define _ZBASE_H_
+
+#include <stdlib.h>
+#include "zgeneric.h"
+
+typedef struct _zbase zbase;
+
+struct _zbase
+{
+	zgeneric parent;				/* inherited parent object */
+	unsigned int _int_flg;			/* internal flag */
+
+
+	/* base coordinates */
+	double x;
+	double y;
+	double ang;						/* orientation angle */
+	double width;					/* with */
+	double height;					/* height */
+	double thk;						/* thickness of material */
+	unsigned int prj_flg;		   	/* projected flag */
+
+	struct _zgeneric_vtable vtable;	/* vtable for update draw, delete and new */
+	zgeneric* super_cls;			/* super class (in this case it will be the parent */
+	void* child;					/* child pointer */
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /* Constructor and destructor */
+    zgeneric* zbase_new(zbase* obj);
+    void zbase_delete(zbase* obj);
+
+    /* Property functions */
+    /*************************************************************/
+
+    /* Set base coordinates */
+    inline __attribute__ ((always_inline)) static int zbase_set_base_coords(zbase* obj, double x, double y)
+    {
+		/* check for object pointers */
+		ZCHECK_OBJ_INT(obj);
+		obj->x = x;
+		obj->y = y;
+		return ZELIA_OK;	
+    }
+
+    /* Set orientation angle */
+    inline __attribute__ ((always_inline)) static int zbase_set_orientation(zbase* obj, double ang)
+    {
+		ZCHECK_OBJ_INT(obj);
+		obj->ang = ang;
+		return ZELIA_OK;
+    }
+    inline __attribute__ ((always_inline)) static double zbase_get_orientation(zbase* obj)
+    {
+		ZCHECK_OBJ_DOUBLE(obj);
+		return obj->ang;
+    }
+
+    /* Set and Get width height */
+    inline __attribute__ ((always_inline)) static int zbase_set_width_and_height(zbase* obj, double width, double height)
+    {
+		/* check for object */
+		ZCHECK_OBJ_INT(obj);
+		obj->width = width;
+		obj->height = height;
+		return ZELIA_OK;
+    }
+    
+    inline __attribute__ ((always_inline)) static int zbase_set_width(zbase* obj, double width)
+    {
+		/* check object */
+		ZCHECK_OBJ_INT(obj);
+		obj->width = width;
+		return ZELIA_OK;
+    }
+    
+    inline __attribute__ ((always_inline)) static double zbase_get_width(zbase* obj)
+    {
+		ZCHECK_OBJ_DOUBLE(obj);
+		return obj->width;
+    }
+
+    inline __attribute__ ((always_inline)) static int zbase_set_height(zbase* obj, double height)
+    {
+		ZCHECK_OBJ_INT(obj);
+		obj->height = height;
+		return 0;
+    }
+    inline __attribute__ ((always_inline)) static double zbase_get_height(zbase* obj)
+    {
+		ZCHECK_OBJ_DOUBLE(obj);
+		return obj->height;
+    }
+
+    inline __attribute__ ((always_inline)) static int zbase_set_thickness(zbase* obj, double thk)
+    {
+		ZCHECK_OBJ_INT(obj);
+		obj->thk = thk;
+		return ZELIA_OK;
+    }
+    inline __attribute__ ((always_inline)) static double zbase_get_thickness(zbase* obj)
+    {
+		ZCHECK_OBJ_DOUBLE(obj);
+		return obj->thk;
+    }
+
+    inline __attribute__ ((always_inline)) static int zbase_set_projected_flag(zbase* obj, unsigned int flg)
+    {
+		ZCHECK_OBJ_INT(obj);
+		obj->prj_flg = flg;
+		return 0;
+    }
+    inline __attribute__ ((always_inline)) static unsigned int zbase_get_projected_flag(zbase* obj)
+    {
+		if(obj == NULL)
+			return 0;
+		else
+			return obj->prj_flg;
+    }
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ZBASE_H_ */
