d2d622175080c4e049f5eb2e284b9d9c1292cc8a
Anoj Perera <anojmperera@gmail.com>
2014-11-30 02:34:35 +0000

    Note class added.

---
+++ src/znote.c
@@ -0,0 +1,222 @@
+/* Implementation of zNote class */
+/* Sat Mar 10 13:25:25 GMT 2012 */
+
+#include "zVar.h"
+#include <stdio.h>
+#include <string.h>
+#include <cairo/cairo.h>
+#include <pango/pangocairo.h>
+
+#include "znote.h"
+
+/* Virtual function */
+static int _znote_draw(void* obj);
+
+/* Constructor */
+zgeneric* znote_new(znote* obj)
+{
+    
+    ZCONSTRUCTOR(obj, znote);
+
+    /* Create parent object */
+    if(!obj->super_cls = zbase_new(&obj->parent))
+	{
+	    if(ZDESTRUCTOR_CHECK)
+		free(obj);
+	    return NULL;
+	}
+
+    /* Set properties */
+    obj->ix = 0;			/* index set to nothing */
+    obj->note[0] = '\0';
+    obj->fnote = NULL;
+    obj->indent = ZNOTE_INDENT;		/* default note index */
+    obj->note_sz = 0;
+
+
+    obj->height_func = NULL;
+    obj->usr_data = NULL;
+    obj->child = NULL;
+    ZGENERIC_INIT_VTABLE(obj);
+
+    /* Set child and function pointers of parent object */
+    zgeneric_set_draw(obj, _znote_draw);
+
+    zgeneric_set_child_pointer(obj);
+
+    /* Return parent object */
+    return obj->super_cls;
+}
+
+
+/* Destructor */
+void znote_delete(znote* obj)
+{
+    ZCHECK_OBJ_VOID(obj);
+
+    if(obj->vtable.zgeneric_delete)
+	obj->vtable.zgeneric_delete((void*) obj->super_cls);
+    
+    /* Call destructor of parent object */
+    zbase_delete(&obj->parent);
+
+    obj->super_cls = NULL;
+    obj->child = NULL;
+    obj->usr_data = NULL;
+    obj->height_func = NULL;
+    ZGENERIC_INIT_VTABLE(obj);
+
+    /* If object was internally created free it */
+    if(ZDESTRUCTOR_CHECK)
+	free(obj);
+    
+    return;
+}
+
+/* Draw method */
+int znote_draw(znote* obj)
+{
+    cairo_t* _dev_c;				/* device context */
+    char* _ix_buff;				/* index buffer */
+    PangoLayout* _layout;			/* pango layout */
+    PangoFontDescription* _desc;    		/* pango font description */
+    double _box_width;
+
+    /* check for object */
+    ZCHECK_OBJ_INT(obj);
+    
+
+    /* Get device context */
+    _dev_c = zgeneric_get_dev_context(obj->super_cls);
+    ZCHECK_OBJ_INT(_dev_c);
+
+    /* check if content was assigned */
+    if(obj->note[0] == '\0' || obj->ix <= 0)
+	return ZELIA_NOTE_ERROR;
+
+    /* get index into a buffer */
+    _ix_buff = (char*) malloc(sizeof(int)+sizeof(char));
+    sprintf(_ix_buff, "%i.", obj->ix);
+    
+    /* save cairo context */
+    cairo_save(_dev_c);
+
+    /* translate to coordinates */
+    cairo_translate(_dev_c,
+		    CONV_TO_POINTS(obj->parent.x),
+		    CONV_TO_POINTS(obj->parent.y));
+
+    _layout = pango_cairo_create_layout(_dev_c);
+    pango_layout_set_width(_layout, CONV_TO_PANGO(obj->indent));
+
+    /* add text to layout */
+    pango_layout_set_text(_layout, _ix_buff, -1);
+
+    /* create font description and add to layout */
+    _desc = pango_font_description_from_string(ZNOTE_FONT_STYLE);
+    pango_layout_set_font_description(_layout, _desc);
+    pango_font_description_free(_desc);
+
+    /* show layout */
+    pango_cairo_show_layout(_dev_c, _layout);
+
+    /* offset cairo context for the text */
+    cairo_translate(_dev_c,
+		    CONV_TO_POINTS(obj->indent),
+		    0.0);
+    /* update pango layout */
+    pango_cairo_update_layout(_dev_c, _layout);
+
+    /* if width was set, tell pango to wrap */
+    _box_width = obj->parent.width - obj->indent;
+    if(obj->parent.width > 0.0)
+	pango_layout_set_width(_layout, CONV_TO_PANGO(_box_width));
+    
+    /* update text */
+    pango_layout_set_text(_layout, obj->note, -1);
+
+    /* show layout */
+    pango_cairo_show_layout(_dev_c, _layout);
+
+    /* If height inform function was set call it */
+    if(obj->height_func)
+	obj->height_func(_generic, obj->usr_data, pango_layout_get_height(_layout));
+
+    /* free layout object */
+    g_object_unref(_layout);
+    free(_ix_buff);
+    
+    /* restore cairo context */
+    cairo_restore(_dev_c);
+
+    _generic = NULL;
+    _dev_c = NULL;
+    _ix_buff = NULL;
+    _layout = NULL;
+    _desc = NULL;
+    
+    return ZELIA_OK;
+}
+
+/*=================================== Property Methods ===================================*/
+
+/* Set content */
+int znote_set_content(znote* obj, const char* content, int ix)
+{
+    /* check object and argument */
+    ZCHECK_OBJ_INT(obj);
+    ZCHECK_OBJ_INT(content);
+
+    obj->ix = ix;
+    obj->note_sz = strlen(content);
+    if(obj->fnote)
+	{
+	    free(obj->fnote);
+	    obj->fnote = NULL;
+	}
+    /* if length exceeds buffer size, return fail */
+    if(obj->note_sz < NOTE_BUFF)
+	strcpy(obj->note, content);
+    else
+	return ZELIA_NOTE_ERROR;
+    
+    return 0;
+}
+
+
+/* Get note and index */
+const char* znote_get_note_with_ix(znote* obj)
+{
+    size_t _t;
+    
+    ZCHECK_OBJ_PTR(obj);
+    if(obj->note[0] == '\0' || obj->ix <= 0)
+	return NULL;
+
+    /* If the fnote was created in a previous call
+     * return it */
+    if(obj->fnote)
+	return obj->fnote;
+
+    _t = sizeof(int) + sizeof(char) * 2 + NOTE_BUFF;
+    obj->fnote = (char*) malloc(_t);
+    
+    /* copy index to buffer */
+    sprintf(obj->fnote, "%i. ", obj->ix);
+    strcat(obj->fnote, obj->note);
+
+    return obj->fnote;
+}
+
+
+/*=================================== Private Methods ===================================*/
+/* Virtual draw method */
+static int _znote_draw(zGeneric* obj)
+{
+    zgeneric* _zg = NULL;
+    
+    ZCHECK_OBJ_INT(obj);
+    _zg = (zgeneric*) obj;
+    
+    return znote_draw(Z_NOTE(_zg));
+}
---
+++ src/znote.h
@@ -0,0 +1,90 @@
+/* zNote class handles an enumerated note. Provides basic interfaces for
+ * adding and numbering notes. Uses pango layout engine. Inherits the
+ * zBase class.
+ * Sat Mar 10 12:59:49 GMT 2012 */
+
+#ifndef _ZNOTE_H_
+#define _ZNOTE_H_
+
+#include <stdlib.h>
+#include "zgeneric.h"
+#include "zbase.h"
+
+#define Z_NOTE_BUFF 256			/* default note size fixed */
+
+/* Forward declaration of struct */
+typedef struct _znote znote;
+
+/* Function pointer for informing final height of
+ * text height */
+typedef int (*znote_fptr)(zgeneric* obj, void* usr_data, int height);
+
+struct _zNote
+{
+    zbase z_parent;			/* Inherited parent object */
+    unsigned int z_int_flg;		/* Internal flag */
+    int z_ix;				/* Note index */
+    char z_note[Z_NOTE_BUFF];		/* Note content buffer */
+    char* z_fnote;			/* Note and index */
+    double z_indent;			/* Indent */
+    size_t z_note_sz;			/* Note length */
+    size_t z_obj_sz;			/* Object size */
+
+    struct _zgeneric_vtable vtable;	/* vtable */
+    znote_fptr z_height_func;		/* Inform function pointer */
+    void* z_usr_data;			/* User data */
+    void* z_child;			/* Child pointer */
+    zgeneric* super_cls;	       	/* super class pointer */
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /* Constructor and destructor */
+    zgeneric* znote_new(znote* obj);
+    void znote_delete(znote* obj);
+
+    /* Draw method */
+    int znote_draw(znote* obj);
+
+    /*******************************************************************/
+    /* Property methods */
+
+    /* Set and get note content */
+    int znote_set_content(znote* obj, const char* content, int ix);
+    inline __attribute__ ((always_inline)) static const char* znote_get_content(znote* obj)
+    {
+	/* check object */
+	ZCHECK_OBJ_PTR(obj);
+
+	if(obj->note[0] != '\0')
+	    return obj->note;
+	else
+	    return NULL;
+    }
+    inline __attribute__ ((always_inline)) static int znote_get_ix(znote* obj)
+    {
+	ZCHECK_OBJ_INT(obj);
+	return obj->ix;
+    }
+    const char* znote_get_note_with_ix(znote* obj);
+
+    /* Set and get indent */
+    inline __attribute__ ((always_inline)) static int znote_set_indent(znote* obj, double indent)
+    {
+	ZCHECK_OBJ_INT(obj);
+	obj->indent = indent;
+	return 0;
+    }
+    inline __attribute__ ((always_inline)) static double znote_get_indent(znote* obj)
+    {
+	ZCHECK_OBJ_DOUBLE(obj);
+	return obj->z_indent;
+    }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ZNOTE_H_ */
