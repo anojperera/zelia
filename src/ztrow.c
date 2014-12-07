af5611632e095cb837ec3a4c70a0c629466ba08e
Anoj Perera <anojmperera@gmail.com>
2014-12-07 17:55:42 +0000

    Default context unauthorise callback method implemented.

--- src/ztrow.c
+++ src/ztrow.c
@@ -6,6 +6,12 @@
 /* Virtual function */
 static int _ztrow_draw(void* obj);
 
+/*
+ * Callback to indicate setting the default context should not be
+ * authorised on this object as this is simply a wrapper of tcell collection.
+ */
+static int _ztrow_unathorise_default(void* obj);
+
 /* Constructor */
 zgeneric* ztrow_new(ztrow* obj, unsigned int ix)
 {
@@ -32,7 +38,8 @@ zgeneric* ztrow_new(ztrow* obj, unsigned int ix)
 
     /* set parent objects function pointers */
     zgeneric_set_draw(obj, _ztrow_draw);
-
+    zgeneric_set_device_auth_default_callback(obj, _ztrow_unathorise_default);
+    
     /* set child pointer of parent object */
     zgeneric_set_child_pointer(obj);
 
@@ -190,4 +197,10 @@ static int _ztrow_draw(void* obj)
 	return _self->vtable.zgeneric_draw(obj);
 
     return _rt;
+}
+
+/* Virtual authorise method */
+static int _ztrow_unathorise_default(void* obj)
+{
+    return ZELIA_UNAUTHORISE;
 }
