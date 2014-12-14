3d4364f7b22cf0c5451a687127e83b703360d587
Anoj Perera <anojmperera@gmail.com>
2014-12-14 14:35:49 +0000

    Parser tokens add for header file.

---
+++ src/zelia_parser.h
@@ -0,0 +1,43 @@
+/*
+ * labels defined for helping the parser
+ */
+
+#ifndef _ZELIA_PARSER_H_
+#define _ZELIA_PARSER_H_
+
+#define ZPARSER_FILE "file"
+#define ZPARSER_TEMPLATE_PATH "template_path"
+#define ZPARSER_SAVE_PATH "save_path"
+#define ZPARSER_SHEET_SIZE "sheet_size"
+#define ZPARSER_SHEET_ATTRIB "attrib"
+#define ZPARSER_TABLE "table"
+#define ZPARSER_COORD_X "x"
+#define ZPARSER_COORD_Y "y"
+#define ZPARSER_TABLE_ROWS "rows"
+#define ZPARSER_TABLE_ROW "row"
+#define ZPARSER_TABLE_COLUMNS "columns"
+#define ZPARSER_COLUMN_WIDTH_ATTRIB "width"
+#define ZPARSER_ROW_HEIGHT_ATTRIB "height"
+#define ZPARSER_ROW_IX_ATTRIB "row_ix"
+#define ZPARSER_COL_IX_ATTRIB "col_ix"
+#define ZPARSER_CONTENT "content"
+#define ZPARSER_NOTES "notes"
+#define ZPARSER_NOTE "note"
+#define ZPARSER_TITLE "title"
+#define ZPARSER_JB "jb"
+#define ZPARSER_BASE "base"
+#define ZPARSER_GLAND "gland"
+#define ZPARSER_GLANDS "glands"
+#define ZPARSER_TERMINAL "terminal"
+#define ZPARSER_TERMINALS "terminals"
+#define ZPARSER_TABLE_TCELL "cell"
+#define ZPARSER_TABLE_CELLS "cells"
+#define ZPARSER_GENERIC "generic"
+#define ZPARSER_GENERICS "generics"
+
+#define ZPARSER_SHEET_A4_PORT "A4_P"
+#define ZPARSER_SHEET_A4_LAND "A4_L"
+#define ZPARSER_SHEET_A3_PORT "A3_P"
+#define ZPARSER_SHEET_A3_LAND "A3_L"
+
+#endif /* _ZELIA_PARSER_H_ */
